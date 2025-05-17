6d2b167511f04d88e19aff56f5169842c24def4b
gayatrilamichhanegithub <gayatrilamichhane8@gmail.com>
2025-05-17 16:25:46 +0545

    Themes extracted

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData LightTheme(BuildContext context) => ThemeData(
+    primarySwatch: Colors.deepPurple,
+    fontFamily: GoogleFonts.lato().fontFamily,
+    appBarTheme: const AppBarTheme(
+      color: Colors.white,
+      elevation: 0.0,
+      iconTheme: IconThemeData(color: Colors.black),
+
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+      toolbarTextStyle: TextStyle(color: Colors.black),
+    ),
+  );
+
+  static ThemeData DarkTheme(BuildContext context) =>
+      ThemeData(brightness: Brightness.dark);
+}
