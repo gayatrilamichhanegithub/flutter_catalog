5cbc5ca4ce642e0c124dfce189897eaa36f1ceb8
gayatrilamichhanegithub <gayatrilamichhane8@gmail.com>
2025-05-18 21:16:56 +0545

    day-12 comp

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,12 +1,15 @@
+import 'dart:convert';
+
 class CatalogModel {
-  static final items = [
+  static List<Item> items = [
     Item(
       id: 1,
       name: "iphone 12 pro",
       desc: "Apple iphone 12th geenration",
       price: 999,
       color: "33505a",
-      image: "https://pngimg.com/uploads/iphone_12/iphone_12_PNG36.png",
+      image:
+          "https://www.citypng.com/public/uploads/preview/hd-apple-blue-iphone-12-pro-pro-max-png-704081694622239gnao0zirej.png",
     ),
   ];
 }
@@ -27,4 +30,78 @@ class Item {
     required this.color,
     required this.image,
   });
+
+  Item copyWith({
+    num? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+  
+    result.addAll({'id': id});
+    result.addAll({'name': name});
+    result.addAll({'desc': desc});
+    result.addAll({'price': price});
+    result.addAll({'color': color});
+    result.addAll({'image': image});
+  
+    return result;
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] ?? 0,
+      name: map['name'] ?? '',
+      desc: map['desc'] ?? '',
+      price: map['price'] ?? 0,
+      color: map['color'] ?? '',
+      image: map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Item &&
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.color == color &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      color.hashCode ^
+      image.hashCode;
+  }
 }
