f593b4e9dcbe7292dcb5da05ba7128ac4a125d56
gayatrilamichhanegithub <gayatrilamichhane8@gmail.com>
2025-05-20 21:35:35 +0545

    day-12 comp

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,17 +1,7 @@
 import 'dart:convert';
 
 class CatalogModel {
-  static List<Item> items = [
-    Item(
-      id: 1,
-      name: "iphone 12 pro",
-      desc: "Apple iphone 12th geenration",
-      price: 999,
-      color: "33505a",
-      image:
-          "https://www.citypng.com/public/uploads/preview/hd-apple-blue-iphone-12-pro-pro-max-png-704081694622239gnao0zirej.png",
-    ),
-  ];
+  static List<Item>? items;
 }
 
 class Item {
@@ -51,14 +41,14 @@ class Item {
 
   Map<String, dynamic> toMap() {
     final result = <String, dynamic>{};
-  
+
     result.addAll({'id': id});
     result.addAll({'name': name});
     result.addAll({'desc': desc});
     result.addAll({'price': price});
     result.addAll({'color': color});
     result.addAll({'image': image});
-  
+
     return result;
   }
 
@@ -85,23 +75,23 @@ class Item {
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-  
+
     return other is Item &&
-      other.id == id &&
-      other.name == name &&
-      other.desc == desc &&
-      other.price == price &&
-      other.color == color &&
-      other.image == image;
+        other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
   }
 
   @override
   int get hashCode {
     return id.hashCode ^
-      name.hashCode ^
-      desc.hashCode ^
-      price.hashCode ^
-      color.hashCode ^
-      image.hashCode;
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson = await rootBundle.loadString(
       "assets/files/catalog.json",
     );
@@ -39,12 +39,45 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(title: Text("Catalog App")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items[index]);
-          },
-        ),
+        child:
+            (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+                ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items![index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.deepPurple),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.black),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: CatalogModel.items?.length,
+                )
+                : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
     );
